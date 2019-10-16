c8c5503a649e7fc49eaf803183fd3aa142ea17d5
newt0n <newt0n@newt0ns-MacBook-Air.local>
2019-10-16 11:52:42 +0530

    Updates

---
+++ precache-manifest.b6e5df2b00cda63a1a717fdc411cc7b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770a77abb88f550b4cd3f915e076390c",
+    "url": "/donate/index.html"
+  },
+  {
+    "revision": "1128540d4809fc04a51e",
+    "url": "/donate/static/css/main.f773dc48.chunk.css"
+  },
+  {
+    "revision": "b310f9aa096f56d6d4fc",
+    "url": "/donate/static/js/2.17c7cecb.chunk.js"
+  },
+  {
+    "revision": "1128540d4809fc04a51e",
+    "url": "/donate/static/js/main.a027e8a7.chunk.js"
+  },
+  {
+    "revision": "cf4dcd008e714d6dea4a",
+    "url": "/donate/static/js/runtime~main.daf08a66.js"
+  }
+]);
